411acef326e7961a1bdc2a5591f77f5b3e65f5ed
TomerIrony <87696247+TomerIrony@users.noreply.github.com>
2022-01-03 10:11:11 +0200

    d

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const { createUser, login, getUser } = require('../controllers/users');
 
-router.post('/signin', login);
+router.post('/signin', auth, login);
 router.post('/signup', createUser);
 router.get('/users/me', auth, getUser);
 
