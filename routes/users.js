b53ad4adb3fb9f0c6cfc353b2b6ba15312cf1a16
TomerIrony <87696247+TomerIrony@users.noreply.github.com>
2022-01-03 10:06:54 +0200

    errors handlers

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const { createUser, login, getUser } = require('../controllers/users');
 
-router.get('/users/me', auth, getUser);
 router.post('/signin', login);
 router.post('/signup', createUser);
+router.get('/users/me', auth, getUser);
 
 module.exports = router;
